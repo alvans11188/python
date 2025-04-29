a502b585838e5d7cf0badba44892805ba6655b63
Alexander <achicallag@unjbg.edu.pe>
2025-04-28 23:23:38 -0500

    condiciones1

---
+++ condicionales1.py
@@ -0,0 +1,8 @@
+# Solicitar la calificación del alumno
+calificacion = int(input("Ingrese la calificación del alumno: "))
+
+# Verificar si el alumno está aprobado o desaprobado
+if calificacion >= 11:
+    print("ALUMNO APROBADO")
+else:
+    print("ALUMNO DESAPROBADO")
