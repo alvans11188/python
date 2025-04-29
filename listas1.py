a166f714bd2cf0cbf18a3c6526a51ed12c27c5ae
Alexander <achicallag@unjbg.edu.pe>
2025-04-28 23:25:07 -0500

    listas
    desconosco aun si son listas y tuplas

---
+++ listas1.py
@@ -0,0 +1,17 @@
+# Crear una lista
+mi_lista = [1, 2, 3, 4, 5]
+
+# Acceder a elementos
+print(mi_lista[0])  # Imprime el primer elemento: 1
+
+# Modificar un elemento
+mi_lista[2] = 10
+print(mi_lista)  # Imprime: [1, 2, 10, 4, 5]
+
+# Agregar elementos
+mi_lista.append(6)
+print(mi_lista)  # Imprime: [1, 2, 10, 4, 5, 6]
+
+# Eliminar elementos
+mi_lista.remove(10)
+print(mi_lista)  # Imprime: [1, 2, 4, 5, 6]
